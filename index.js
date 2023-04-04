c4a53baefca50319ef5fad520c9f236113757132
Shania Lopez <slopez@fandm.edu>
2023-04-03 20:00:49 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Code your solution in this file!
+
+const companyName = 'Scuber'; 
+
+let mostProfitableNeighborhood;
+    mostProfitableNeighborhood = 'Chelsea';
+
+let companyCeo;
+    companyCeo = 'Susan Smith';
